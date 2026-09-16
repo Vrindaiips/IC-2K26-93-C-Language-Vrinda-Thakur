0611640ce7d2c0dc0b1b000e950103fde7336b76
Vrindaiips <vrindat70@gmail.com>
2026-09-16 23:26:56 +0530

    Create Remainder.c

---
+++ LAB_02/Remainder/Remainder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("Vrinda Thakur\nEnter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    c=a%b;
+    printf("Remainder Is:");
+    printf("%d",c);
+    return 0;
+} 
