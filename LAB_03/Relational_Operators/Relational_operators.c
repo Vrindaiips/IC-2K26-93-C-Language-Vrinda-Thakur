b91e46e4c1a2ed6719880f8b6f27948bad7a4f27
Vrindaiips <vrindat70@gmail.com>
2026-09-18 14:37:38 +0530

    Create Relational_operators.c

---
+++ LAB_03/Relational_Operators/Relational_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=25;
+    int b=15;
+    printf("Vrinda Thakur\n");
+    printf("%d\n", a==b);
+    printf("%d\n", a>b);
+    printf("%d\n", a<b);
+    printf("%d\n", a<=b);
+    printf("%d\n", a>=b);
+    printf("%d\n", a!=b);
+    return 0;
+
+}
