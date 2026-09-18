b8cc0388da27c8114e17e88aa5c5db1373cfcc23
Vrindaiips <vrindat70@gmail.com>
2026-09-18 15:02:14 +0530

    Create logical.c

---
+++ LAB_03/Logical_Operators/logical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("Vrinda Thakur\nEnter First Number:");
+    scanf("%d",&a);
+    printf("Enter Second Number:");
+    scanf("%d",&b);
+    printf("Result of '&&'operator:%d\n", a&&b);
+    printf("Result of '|' operator:%d\n", a|b);
+    printf("Result of '!' operator:%d\n", !a);
+    return 0;
+   
+}
